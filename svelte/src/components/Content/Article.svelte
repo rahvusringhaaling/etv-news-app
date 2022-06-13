73a7e3b7d9eda6f757ef5443801ebbf1348eb392
Oliver Ilp <oliver.ilp@err.ee>
2022-06-13 14:51:58 +0300

    Refactor

--- svelte/src/components/Content/Article.svelte
+++ svelte/src/components/Content/Article.svelte
@@ -4,11 +4,16 @@
   import { ScheduleType } from '../../domain/IScheduleItem';
   import { sleep } from '../../utils';
   import PageIndicator from './PageIndicator.svelte';
+  import type { IArticleNodes } from '../../domain/IArticleNodes';
 
   let lead = '';
   let body = '';
   let bodyContainer: HTMLElement;
   const maxHeight = 950;
+  export let articleNodes: IArticleNodes = {
+    usedElements: [],
+    availableElements: []
+  };
 
   function getInnermostChild(element: ChildNode): ChildNode {
     return element.hasChildNodes() &&
@@ -17,41 +22,50 @@
       : element;
   }
 
+  function addBodyChildren() {
+    const newChildren = [...bodyContainer.childNodes].map((element) =>
+      element.nodeType === Node.TEXT_NODE
+        ? document.createElement('br')
+        : element
+    ) as HTMLElement[];
+    articleNodes.availableElements = [...newChildren];
+    bodyContainer.replaceChildren();
+
+    for (const candidate of newChildren) {
+      bodyContainer.appendChild(candidate);
+
+      const { y } = candidate.getBoundingClientRect();
+      if (y > maxHeight) {
+        candidate.remove();
+        break;
+      }
+
+      articleNodes.availableElements.shift();
+      articleNodes.usedElements.push(candidate);
+
+      const child = getInnermostChild(candidate);
+      if (child.firstChild?.nodeType === Node.TEXT_NODE) {
+        const words = child.textContent!.split(' ');
+        // console.log(words);
+        for (let i = words.length; i >= 0; i--) {
+          const { y, height } = candidate.getBoundingClientRect();
+          if (y + height <= maxHeight) {
+            break;
+          }
+          child.textContent = words.slice(0, i).join(' ');
+        }
+      }
+    }
+  }
+
   const unsubscribe = current.subscribe(async (item) => {
     if (item && item.type === ScheduleType.Headline) {
       await sleep(1000);
-      lead = item.article!.lead;
-      body = item.article!.body;
-      await sleep(0);
-
-      const newChildren = [...bodyContainer.childNodes].map((element) =>
-        element.nodeType === Node.TEXT_NODE
-          ? document.createElement('br')
-          : element
-      ) as HTMLElement[];
-      bodyContainer.replaceChildren();
-
-      for (const candidate of newChildren) {
-        bodyContainer.appendChild(candidate);
-
-        const { y } = candidate.getBoundingClientRect();
-        if (y > maxHeight) {
-          candidate.remove();
-          break;
-        }
-
-        const child = getInnermostChild(candidate);
-        if (child.firstChild?.nodeType === Node.TEXT_NODE) {
-          const words = child.textContent!.split(' ');
-          // console.log(words);
-          for (let i = words.length; i >= 0; i--) {
-            const { y, height } = candidate.getBoundingClientRect();
-            if (y + height <= maxHeight) {
-              break;
-            }
-            child.textContent = words.slice(0, i).join(' ');
-          }
-        }
+      if (articleNodes.usedElements.length === 0) {
+        lead = item.article!.lead;
+        body = item.article!.body;
+        await sleep(0);
+        addBodyChildren();
       }
     }
   });
